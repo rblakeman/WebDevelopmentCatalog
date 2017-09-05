f3e449e44d0e688af93fa598666adbde8049ba6f
Ryan Blakeman <rblakeman@csumb.edu>
2017-09-05 21:26:20 +0000

    Fixing Heroku

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="./style.css">
+    </head>
+    <body>
+        <h1 id:"title">
+            CST 336 Assignments - Ryan Blakeman
+        </h1>
+        <u1 id:"labmenu">
+            Labs
+            <li>
+            <a href="lab1/structure.html">Lab 1</a>
+            <p></p>
+        </u1>    
+        <u1 id:"hwmenu">
+            Homeworks
+            <li>
+            <a href="hw1/structure.html">Homework 1</a>    
+            </li>
+        </u1>
+    </body>
+</html>
