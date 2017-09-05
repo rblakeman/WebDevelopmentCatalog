a6546b08218ebab2075a333bb7864935d6b36484
Ryan Blakeman <rblakeman@csumb.edu>
2017-09-05 21:35:52 +0000

    Fixing Heroku again..

--- index.php
+++ index.php
@@ -9,13 +9,13 @@
         <u1 id:"labmenu">
             Labs
             <li>
-            <a href="lab1/structure.html">Lab 1</a>
+            <a href="lab1/index.html">Lab 1</a>
             <p></p>
         </u1>    
         <u1 id:"hwmenu">
             Homeworks
             <li>
-            <a href="hw1/structure.html">Homework 1</a>    
+            <a href="hw1/index.html">Homework 1</a>    
             </li>
         </u1>
     </body>
