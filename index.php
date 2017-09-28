574968ef8fb39bda5d4d3ffa746d12858615c19f
Ryan Blakeman <rblakeman@csumb.edu>
2017-09-28 22:09:20 +0000

    empty hw3 lab4 indices

--- index.php
+++ index.php
@@ -11,12 +11,14 @@
             <li> <a href="lab1/index.html">Lab 1</a> </li>
             <li> <a href="lab2/index.php">Lab 2</a> </li>
             <li> <a href="lab3/index.php">Lab 3</a> </li>
+            <li> <a href="lab4/index.php">Lab 4</a> </li>
             <p></p>
         </u1>
         <u1 id:"hwmenu">
             Homeworks
             <li> <a href="hw1/index.html">Homework 1</a> </li>
             <li> <a href="hw2/index.php">Homework 2</a> </li>
+            <li> <a href="hw3/index.php">Homework 3</a> </li>
         </u1>
     </body>
 </html>
