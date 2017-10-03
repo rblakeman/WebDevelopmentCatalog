efd296e837c22f5fcc3e86f226c7d09db6d0ba17
Ryan Blakeman <rblakeman@csumb.edu>
2017-10-03 20:48:02 +0000

    Finished lab4!

--- hw3/index.php
+++ hw3/index.php
@@ -1,4 +1,36 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Print Machine</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <br>
+        <main>
+            <form action="Print" method="post">
+                <input type="text" name="tex" placeholder="What to print?" value="<?=$_GET['tex']?>"/>
+                <input type = "radio" id = "lefttoright" name = "order" value = "normal">
+                    <label for = "normal"></label><label for="lefttoright"> Normal </label>
+                <input type= "radio" id= "righttoleft" name = "order" value = "reversed">
+                    <label for = "reversed"></label><label for ="righttoleft"> Reversed </label>
+                <br>
+                
+                <input type="number" name="red" placeholder="255" value="num" min="0" max="255">Red
+                <input type="number" name="green" placeholder="255" value="num" min="0" max="255">Green
+                <input type="number" name="blue" placeholder="255" value="num" min="0" max="255">Blue
+                <br>
+                
+                <input type="checkbox" name="bold" value="boldvalue" />Bold
+                <input type="checkbox" name="italic" value="italicvalue" />Italic
+                <input type="checkbox" name="underline" value="underlinevalue" />Underline
+                <input type="submit" name="formSubmit" value="Submit" />
+            </form>
+
+        </main>
+        <footer>
+            
+        </footer>
+    </body>
     
 </html>
--- lab4/index.php
+++ lab4/index.php
@@ -28,7 +28,7 @@
     <head>
         <title>Image Carousel</title>
         <meta charset="utf-8" />
-        <link href="css/style.css" rel="stylesheet" type="text/css" />
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <style>
             @import url("css/style.css");
