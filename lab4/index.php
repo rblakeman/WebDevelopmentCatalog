3f57b832f73e10378f6401eafcaba8d506076781
Ryan Blakeman <rblakeman@csumb.edu>
2017-10-03 07:28:25 +0000

    lab4 progress...

--- lab4/index.php
+++ lab4/index.php
@@ -1,46 +1,18 @@
 <?php
-    //returns array with 100 URLs to images from Pixabay.com, based on a "keyword"
-    function getImageURLs($keyword, $orientation="horizontal") {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => "https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&q=$keyword&image_type=photo&orientation=$orientation&safesearch=true&per_page=100",
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_TIMEOUT => 30,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => "GET",
-          CURLOPT_HTTPHEADER => array(
-            "cache-control: no-cache"
-          ),
-        ));
-        
-        $jsonData = curl_exec($curl);
-        $data = json_decode($jsonData, true); //true makes it an array!
-        
-        $imageURLs = array();
-        for ($i = 0; $i < 99; $i++) {
-        $imageURLs[] = $data['hits'][$i]['webformatURL'];
-        }
-        $err = curl_error($curl);
-        curl_close($curl);
-        
-        return $imageURLs;
-    }
-
     $backgroundImage = "img/sea.jpg";
+    include 'api/pixabayAPI.php';
     
     if(isset($_GET['keyword']))
-        include 'api/pixabayAPI.php';
         $keyword = $_GET['keyword'];
-        $orientation = "horizontal";
-        $imageURLs = getImageURLs($keyword, $orientation);
+        $imageURLs = getImageURLs($keyword);
         $backgroundImage = $imageURLs[array_rand($imageURLs)];
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8" />
         <title>Image Carousel</title>
+        <meta charset="utf-8" />
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <style>
             @import url("css/style.css");
@@ -60,8 +32,8 @@
             <ol class="carousel-indicators">
                 <?php
                     for($i = 0; $i < 7; $i++) {
-                        echo "<li data-targe'#carousel-example-generic' data-slide-to='$i'";
-                        echo ($i == 0) ? "class='active'" : " ";
+                        echo "<li data-target='#carousel-example-generic' data-slide-to='$i'";
+                        echo ($i == 0)?" class='active'" : " ";
                         echo "></li>";
                     }
                 ?>
@@ -74,23 +46,23 @@
                         } while(!isset($imageURLs[$randomIndex]));
                         
                         echo '<div class="item ';
-                        echo ($i==0) ? "active" : "";
+                        echo ($i == 0) ? "active" : "";
                         echo '">';
                         echo '<img src="' . $imageURLs[$randomIndex] . '">';
-                        echo '</div';
+                        echo '</div>';
                         unset($imageURLs[$randomIndex]);
                     }
                 ?>
             </div>
             
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev"?>
-            <span class="glyphicon glyphicpn-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-            <span class="glyphicon glyphicpn-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
         <?php
             }
@@ -100,12 +72,14 @@
             <input type="text" name="keyword" placeholder="keyword" value="<?=$_GET['keyword']?>"/>
             <input type = "radio" id = "lhorizontal" name = "layout" value = "horizontal">
             <label for = "Horizontal"></label><label for="lhorizontal"> Horizontal </label>
-            <input type= "raido" id= "lvertical" name = "layout" value = "vertical">
-            <label for = "Vetical"></label><label for ="lvertical"> Vetical </label>
+            <input type= "radio" id= "lvertical" name = "layout" value = "vertical">
+            <label for = "Vertical"></label><label for ="lvertical"> Vertical </label>
             <select name = "category">
                 <option value ="">Select One</option>
-                <option value="ocean">Sea</option>
+                <option value="ocean">Bay</option>
                 <option>Forest</option>
+                <option>Mars</option>
+                <option>Snow</option>
             </select>
             <input type="submit" value="Search"/>
         </form>
