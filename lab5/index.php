ce6ffa390f64ee05411be223a3c4b05d0589f756
Ryan Blakeman <rblakeman@csumb.edu>
2017-10-20 05:50:13 +0000

    More lab5 progress

--- lab5/index.php
+++ lab5/index.php
@@ -8,10 +8,32 @@
     <body>
         <nav>
             <form>
-                <input type="radio" id="lavailable" name="status" value="available">
-                <label for="Available"></label><label for="lavailable"> Available </label>
-                <input type="radio" id="lcheckedout" name="status" value="checkedout">
-                <label for="CheckedOut"></label><label for="lcheckedout"> CheckedOut </label>
+                <span id="Filter">Sort - </span>
+                <select name = "sort">
+                    <option value="name">Name</option>
+                    <option value="nascending">-->Ascending</option>
+                    <option value="ndescending">-->Descending</option>
+                    <option value="type">Price</option>
+                    <option value="tascending">-->Ascending</option>
+                    <option value="tdescending">-->Descending</option>
+                </select>
+                <br>
+                
+                <span id="Filter">Filter - </span>
+                Name:
+                <input type="text" name="name" placeholder="Product Name" value="<?=$_GET['name']?>"/>
+                Status:
+                <input type="checkbox" name="status" value="available" checked>Available
+                <input type="checkbox" name="status" value="checkedout" checked>Checked Out
+                <br>
+                Type:
+                <input type="checkbox" name="type" value="tablet" checked>Tablet
+                <input type="checkbox" name="type" value="camera" checked>Camera
+                <input type="checkbox" name="type" value="vrheadset" checked>VR Headset
+                <input type="checkbox" name="type" value="webcam" checked>Webcam
+                <input type="checkbox" name="type" value="smartphone" checked>Smartphone
+                <input type="checkbox" name="type" value="laptop" checked>Laptop
+                <input type="checkbox" name="type" value="microphone" checked>Microphone
             </form>
         </nav>
         <?php
@@ -22,41 +44,53 @@
             $dbConn = new PDO("mysql:host=$host; dbname=$dbname", $username, $password);
             $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
-            $dbData = $dbConn->query("SELECT * FROM device"); //WHERE deviceType = 'Camera'"); // gets data base object
-            $dbArray = $dbData->fetchAll(); // gets associative array of data
-            ?><div id="bar">
-                <div id="names">Name</div>
-                <div id="types">Type</div>
-                <div id="available">Status</div>
-                <br>
-            </div>
+            $dbData = $dbConn->query("SELECT * FROM device ORDER BY deviceName ASC");
+            if ($_GET['sort'] == "name" || $_GET['sort'] == "nascending")
+                $dbData = $dbConn->query('SELECT * FROM device ORDER BY deviceName ASC');
+            else if ($_GET['sort'] == "ndescending")
+                $dbData = $dbConn->query('SELECT * FROM device ORDER BY deviceName DESC');
+            
+            else if ($_GET['sort'] == "price" || $_GET['sort'] == "pascending")
+                $dbData = $dbConn->query('SELECT * FROM device ORDER BY price ASC');
+            else if ($_GET['sort'] == "pdescending")
+                $dbData = $dbConn->query('SELECT * FROM device ORDER BY price DESC');
+            
+            $dbArray = $dbData->fetchAll();
+            ?><div id="bar"><div id="row2">
+                <div id="column2">Name</div>
+                <div id="column2">Type</div>
+                <div id="column2">Price</div>
+                <div id="column2">Status</div>
+            </div></div>
             <?php
             for ($i = 0; $i < sizeof($dbArray); $i++)
             {
+                ?><div id="whitecol">
+                <div id="row"><?php
                 if ($i % 2 == 0) {
-                    ?><div id="whitecol">
-                    <div id="names"><?php
+                    ?><div id="column"><?php
                     echo $dbArray[$i]['deviceName'];
-                    ?></div><div id="types"><?php
+                    ?></div><div id="column"><?php
                     echo $dbArray[$i]['deviceType'];
-                    ?></div><div id="available"><?php
+                    ?></div><div id="column"><?php
+                    echo $dbArray[$i]['price'];
+                    ?></div><div id="column"><?php
                     echo $dbArray[$i]['status'];
-                    ?></div><?php
-                    echo "<br>";
-                    ?></div><?php
+                    ?></div></div><?php
+                    ?><div id="greycol"><?php
                 }
                 else {
-                    ?><div id="greycol">
-                    <div id="names"><?php
+                    ?><div id="column"><?php
                     echo $dbArray[$i]['deviceName'];
-                    ?></div><div id="types"><?php
+                    ?></div><div id="column"><?php
                     echo $dbArray[$i]['deviceType'];
-                    ?></div><div id="available"><?php
+                    ?></div><div id="column"><?php
+                    echo $dbArray[$i]['price'];
+                    ?></div><div id="column"><?php
                     echo $dbArray[$i]['status'];
-                    ?></div><?php
-                    echo "<br>";
-                    ?></div><?php
+                    ?></div></div><?php
                 }
+                ?></div><?php
             }
             
         ?>
