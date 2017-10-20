39c1c86239489e566abdf992c61cd6a5e329ce06
Ryan Blakeman <rblakeman@csumb.edu>
2017-10-20 06:36:08 +0000

    added filters and the such but not finished

--- lab5/index.php
+++ lab5/index.php
@@ -6,6 +6,7 @@
         <link href="css/style.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
+        <div id="title">Tech Checkout</div>
         <nav>
             <form>
                 <span id="Filter">Sort - </span>
@@ -17,23 +18,24 @@
                     <option value="tascending">-->Ascending</option>
                     <option value="tdescending">-->Descending</option>
                 </select>
+                <input type="submit" name="submit" value="Submit">
                 <br>
                 
                 <span id="Filter">Filter - </span>
                 Name:
-                <input type="text" name="name" placeholder="Product Name" value="<?=$_GET['name']?>"/>
+                <input type="text" name="typedtext" placeholder="Product Name" value="<?=$_GET['typedtext']?>"/>
                 Status:
-                <input type="checkbox" name="status" value="available" checked>Available
-                <input type="checkbox" name="status" value="checkedout" checked>Checked Out
+                <input type="checkbox" name="available" value="true">Available
+                <input type="checkbox" name="checkedout" value="true">Checked Out
                 <br>
                 Type:
-                <input type="checkbox" name="type" value="tablet" checked>Tablet
-                <input type="checkbox" name="type" value="camera" checked>Camera
-                <input type="checkbox" name="type" value="vrheadset" checked>VR Headset
-                <input type="checkbox" name="type" value="webcam" checked>Webcam
-                <input type="checkbox" name="type" value="smartphone" checked>Smartphone
-                <input type="checkbox" name="type" value="laptop" checked>Laptop
-                <input type="checkbox" name="type" value="microphone" checked>Microphone
+                <input type="checkbox" name="tablet" value="true">Tablet
+                <input type="checkbox" name="camera" value="true">Camera
+                <input type="checkbox" name="vrheadset" value="true">VR Headset
+                <input type="checkbox" name="webcam" value="true">Webcam
+                <input type="checkbox" name="smartphone" value="true">Smartphone
+                <input type="checkbox" name="laptop" value="true">Laptop
+                <input type="checkbox" name="microphone" value="true">Microphone
             </form>
         </nav>
         <?php
@@ -43,18 +45,47 @@
             $password = '1cff1392';
             $dbConn = new PDO("mysql:host=$host; dbname=$dbname", $username, $password);
             $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $dispatch = "SELECT * FROM device ";
             
-            $dbData = $dbConn->query("SELECT * FROM device ORDER BY deviceName ASC");
-            if ($_GET['sort'] == "name" || $_GET['sort'] == "nascending")
-                $dbData = $dbConn->query('SELECT * FROM device ORDER BY deviceName ASC');
+            # Filter
+            if (!empty($_GET['name'])); # Name
+                #$dispatch = $dispatch . "WHERE deviceName == '" . $_GET['device-name'] . "'";
+            
+            if ($_GET['available'] == "true") # Status
+                $dispatch = $dispatch . "WHERE status == 'Available' OR ";
+            if ($_GET['checkedout'] == "true")
+                $dispatch = $dispatch . "WHERE status == 'CheckedOut' OR ";
+                
+            if ($_GET['tablet'] == "true") # Type
+                $dispatch = $dispatch . "WHERE deviceType == available OR ";
+            if ($_GET['camera'] == "true")
+                $dispatch = $dispatch . "WHERE deviceType == available OR ";
+            if ($_GET['vrheadset'] == "true")
+                $dispatch = $dispatch . "WHERE deviceType == available OR ";
+            if ($_GET['webcam'] == "true")
+                $dispatch = $dispatch . "WHERE deviceType == available OR ";
+            if ($_GET['smartphone'] == "true")
+                $dispatch = $dispatch . "WHERE deviceType == available OR ";
+            if ($_GET['laptop'] == "true")
+                $dispatch = $dispatch . "WHERE deviceType == available OR ";
+            if ($_GET['microphone'] == "true")
+                $dispatch = $dispatch . "WHERE deviceType == available OR ";
+            
+            # Sort
+            if ($_GET['sort'] == "name" || $_GET['sort'] == "nascending") # Name
+                $dispatch = $dispatch . "ORDER BY deviceName ASC";
             else if ($_GET['sort'] == "ndescending")
-                $dbData = $dbConn->query('SELECT * FROM device ORDER BY deviceName DESC');
-            
-            else if ($_GET['sort'] == "price" || $_GET['sort'] == "pascending")
-                $dbData = $dbConn->query('SELECT * FROM device ORDER BY price ASC');
+                $dispatch = $dispatch . "ORDER BY deviceName DESC";
+            else if ($_GET['sort'] == "price" || $_GET['sort'] == "pascending") # Price
+                $dispatch = $dispatch . "ORDER BY price ASC";
             else if ($_GET['sort'] == "pdescending")
-                $dbData = $dbConn->query('SELECT * FROM device ORDER BY price DESC');
+                $dispatch = $dispatch . "ORDER BY price DESC";
+            else # Default Case
+                $dispatch = $dispatch . "ORDER BY deviceName ASC";
             
+            echo $dispatch;
+            #$dispatch = "SELECT * FROM device ORDER BY deviceName ASC";
+            $dbData = $dbConn->query($dispatch);
             $dbArray = $dbData->fetchAll();
             ?><div id="bar"><div id="row2">
                 <div id="column2">Name</div>
@@ -65,32 +96,15 @@
             <?php
             for ($i = 0; $i < sizeof($dbArray); $i++)
             {
-                ?><div id="whitecol">
-                <div id="row"><?php
-                if ($i % 2 == 0) {
-                    ?><div id="column"><?php
-                    echo $dbArray[$i]['deviceName'];
-                    ?></div><div id="column"><?php
-                    echo $dbArray[$i]['deviceType'];
-                    ?></div><div id="column"><?php
-                    echo $dbArray[$i]['price'];
-                    ?></div><div id="column"><?php
-                    echo $dbArray[$i]['status'];
-                    ?></div></div><?php
-                    ?><div id="greycol"><?php
-                }
-                else {
-                    ?><div id="column"><?php
-                    echo $dbArray[$i]['deviceName'];
-                    ?></div><div id="column"><?php
-                    echo $dbArray[$i]['deviceType'];
-                    ?></div><div id="column"><?php
-                    echo $dbArray[$i]['price'];
-                    ?></div><div id="column"><?php
-                    echo $dbArray[$i]['status'];
-                    ?></div></div><?php
-                }
-                ?></div><?php
+                ?><div id="row"><div id="column"><?php
+                echo $dbArray[$i]['deviceName'];
+                ?></div><div id="column"><?php
+                echo $dbArray[$i]['deviceType'];
+                ?></div><div id="column"><?php
+                echo $dbArray[$i]['price'];
+                ?></div><div id="column"><?php
+                echo $dbArray[$i]['status'];
+                ?></div></div><?php
             }
             
         ?>
